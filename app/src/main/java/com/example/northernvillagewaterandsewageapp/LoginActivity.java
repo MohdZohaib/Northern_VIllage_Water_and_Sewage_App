e0313d9afe247d4ec909e23e50d64238c3f161a9
MZohaib <37748921+MohdZohaib@users.noreply.github.com>
2020-03-01 20:08:50 -0500

    All but driver activity committed.

--- app/src/main/java/com/example/northernvillagewaterandsewageapp/LoginActivity.java
+++ app/src/main/java/com/example/northernvillagewaterandsewageapp/LoginActivity.java
@@ -18,7 +18,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         userName = findViewById(R.id.UserNameText);
         userPin = findViewById(R.id.PinText);
